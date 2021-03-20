8162679c2fac119801979c4279ef8eb62e55d117
Siddhartha Sarkar <siddsarkar7@gmail.com>
2021-03-20 17:51:07 +0530

    done

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ document.getElementById("calculate-btn").onclick = () => {
   let truckVol = document.getElementById("vol-tr").valueAsNumber;
   let truckTransVel = document.getElementById("trans-vel").valueAsNumber;
   let truckOprEff = document.getElementById("oe").valueAsNumber;
-  let truckTraffic = document.getElementById("tr-traffic").valueAsNumber;
+  // let truckTraffic = document.getElementById("tr-traffic").valueAsNumber;
   let truckTransDist = document.getElementById("trans-dist").valueAsNumber;
   let truckReturningVel = document.getElementById("re-vel").valueAsNumber;
   let truckDumpTime = document.getElementById("dump-time").valueAsNumber;
@@ -24,6 +24,9 @@ document.getElementById("calculate-btn").onclick = () => {
   let truckShiftChngTime = document.getElementById("shift-chng-time")
     .valueAsNumber;
   let otherTime = document.getElementById("other-time").valueAsNumber;
+
+  //result display
+  let res = document.getElementById("result-items");
 
   let bucketFactor = document.querySelectorAll('input[name="bucket-factor"]');
   let realVolCapShovel;
@@ -48,6 +51,7 @@ document.getElementById("calculate-btn").onclick = () => {
   let totalProdTruck;
   let matchFactor;
   let queueLength;
+  let bucketFillFactor;
   let y, u;
   for (let i = 0; i < bucketFactor.length; i++) {
     if (bucketFactor[i].checked) {
@@ -56,7 +60,7 @@ document.getElementById("calculate-btn").onclick = () => {
   }
 
   realVolCapShovel = volShovel * bucketFillFactor;
-  console.log("realVolCapShovel:", realVolCapShovel);
+  console.log("realVolCapShovel", realVolCapShovel);
 
   bulkDensity = specificGravity / swellingFactor;
   console.log("bulkDensity:", bulkDensity);
@@ -66,8 +70,11 @@ document.getElementById("calculate-btn").onclick = () => {
 
   numBucketPertruck = Math.floor(truckVol / realVolCapShovel); // is an integer
 
-  overallEffi =
-    (utilisation * mechAvailability * truckTraffic * truckOprEff) / 100000000;
+  // let nbpt = document.createElement("li");
+  // nbpt.innerHTML = "Number of Buckets per Truck: " + numBucketPertruck;
+  // res.appendChild(nbpt);
+
+  overallEffi = (utilisation * mechAvailability * truckOprEff) / 1000000;
   console.log("overallEffi:", overallEffi * 100);
 
   hourlyProd =
@@ -130,7 +137,8 @@ document.getElementById("calculate-btn").onclick = () => {
     retTime +
     truckSamplingTime +
     truckBedClnTime +
-    truckManuvTime;
+    truckManuvTime +
+    otherTime;
   console.log("cycleTimeTruck", cycleTimeTruck);
 
   numTripShift = Math.floor(
@@ -145,7 +153,7 @@ document.getElementById("calculate-btn").onclick = () => {
   console.log("prodTruckDay", prodTruckDay);
 
   numTruckDay = Math.floor((prodPerDay * numShovelReq) / prodTruckDay);
-  console.log("numTruckDay", numTruckDay);
+  console.log("numTruckDay:", numTruckDay);
 
   totalProdTruck = prodTruckDay * numTruckDay;
   console.log("totalProdTruck", totalProdTruck);
@@ -158,4 +166,108 @@ document.getElementById("calculate-btn").onclick = () => {
 
   queueLength = (y * y) / (u * (u - y));
   console.log("queueLength", queueLength);
+
+  //DISPLAY RESULTS
+
+  let rvcs = document.createElement("li");
+  rvcs.innerHTML =
+    "Real Volumetric capacity of the Shovel Bucket: " +
+    realVolCapShovel +
+    " cubic meter";
+  res.appendChild(rvcs);
+
+  let bd = document.createElement("li");
+  bd.innerHTML = "Bulk Density: " + bulkDensity + " tonnes/cubic meter";
+  res.appendChild(bd);
+
+  let rbc = document.createElement("li");
+  rbc.innerHTML =
+    "Real Capacity of the Bucket: " + realBucketCapacity + " tonnes";
+  res.appendChild(rbc);
+
+  let oe = document.createElement("li");
+  oe.innerHTML =
+    "Overall Efficiency of the Shovel and Dumper Combination: " +
+    overallEffi * 100 +
+    " %";
+  res.appendChild(oe);
+
+  let hp = document.createElement("li");
+  hp.innerHTML =
+    "Hourly Productivity from Shovel: " + hourlyProd + " tonnes/hour";
+  res.appendChild(hp);
+
+  let pps = document.createElement("li");
+  pps.innerHTML = "prodPerShift:" + prodPerShift;
+  res.appendChild(pps);
+
+  let ppd = document.createElement("li");
+  ppd.innerHTML = "prodPerDay:" + prodPerDay;
+  res.appendChild(ppd);
+
+  let nsr = document.createElement("li");
+  nsr.innerHTML = "numShovelReq:" + numShovelReq;
+  res.appendChild(nsr);
+
+  let tps = document.createElement("li");
+  tps.innerHTML = "totalProdShovel:" + totalProdShovel;
+  res.appendChild(tps);
+
+  let evt = document.createElement("li");
+  evt.innerHTML = "estVolTruck:" + estVolTruck;
+  res.appendChild(evt);
+
+  let ebt = document.createElement("li");
+  ebt.innerHTML = "estBulkTon:" + estBulkTon;
+  res.appendChild(ebt);
+
+  let nbpt = document.createElement("li");
+  nbpt.innerHTML = "numBucketPertruck:" + numBucketPertruck;
+  res.appendChild(nbpt);
+
+  let lt = document.createElement("li");
+  lt.innerHTML = "loadTime:" + loadTime;
+  res.appendChild(lt);
+
+  let tt = document.createElement("li");
+  tt.innerHTML = "transTime:" + transTime;
+  res.appendChild(tt);
+
+  let rt = document.createElement("li");
+  rt.innerHTML = "retTime:" + retTime;
+  res.appendChild(rt);
+
+  let ctt = document.createElement("li");
+  ctt.innerHTML = "cycleTimeTruck:" + cycleTimeTruck;
+  res.appendChild(ctt);
+
+  let nts = document.createElement("li");
+  nts.innerHTML = "numTripShift:" + numTripShift;
+  res.appendChild(nts);
+
+  let pts = document.createElement("li");
+  pts.innerHTML = "prodTruckShift:" + prodTruckShift;
+  res.appendChild(pts);
+
+  let ptd = document.createElement("li");
+  ptd.innerHTML = "prodTruckShift:" + prodTruckShift;
+  res.appendChild(ptd);
+
+  let ntd = document.createElement("li");
+  ntd.innerHTML = "numTruckDay:" + numTruckDay;
+  res.appendChild(ntd);
+
+  let tpt = document.createElement("li");
+  tpt.innerHTML = "totalProdTruck:" + totalProdTruck;
+  res.appendChild(tpt);
+
+  let mf = document.createElement("li");
+  mf.innerHTML = "matchFactor:" + matchFactor;
+  res.appendChild(mf);
+
+  let ql = document.createElement("li");
+  ql.innerHTML = "queueLength:" + queueLength;
+  res.appendChild(ql);
+
+  document.getElementById("results-tab").scrollIntoView(true);
 };
